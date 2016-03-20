84aaf89898116774fb58a1867ee7568ff5af8504
Leopoldo Barreiro <leopoldo.java@gmail.com>
2016-03-20 19:46:23 -0300

    Desabilitado debug mode.

--- fonts/botgram/src/main/java/org/javaleo/libs/botgram/service/BotGramService.java
+++ fonts/botgram/src/main/java/org/javaleo/libs/botgram/service/BotGramService.java
@@ -39,7 +39,7 @@ public class BotGramService implements IBotGramService {
 	public static final String ACCEPT = "Accept";
 	public static final String CONTENT_TYPE = "Content-Type";
 	public static final String JSON_APPLICATION = "application/json; charset=UTF-8";
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 
 	private IBotGramConfig botGramConfig;
 
