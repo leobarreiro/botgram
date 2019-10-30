4a4d1550642d2ee2f303c61d7dec0998372625e8
Leopoldo Barreiro <leopoldo.java@gmail.com>
2019-10-30 17:36:36 -0300

    Including lombok annotations

--- src/main/java/org/javaleo/libs/botgram/model/Poll.java
+++ src/main/java/org/javaleo/libs/botgram/model/Poll.java
@@ -6,6 +6,19 @@ import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
 public class Poll implements Serializable {
 
 	private static final long serialVersionUID = 1L;
